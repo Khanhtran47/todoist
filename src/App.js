5851032a598380df6a8f36b186d8016336624baa
Khanhtran47 <tranduckhanh23@gmail.com>
2020-09-28 00:14:07 +0200

    building the sidebar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+
 import { Header } from './components/header';
+import { Content } from './components/content';
 
 export const App = () => (
   <div className="App">
     <Header />
+    <Content />
   </div>
 );
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ import { FaPizzaSlice } from 'react-icons/fa';
 
 export const Header = () => {
   return (
-    <header className="header" data-testId="header">
+    <header className="header" data-testid="header">
       <nav>
         <div className="logo">
           <img src="images/logo.png" alt="Todoist" />
