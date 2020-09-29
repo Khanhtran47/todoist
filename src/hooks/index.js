425b6aaf2872302806587c56815367d1f594999c
Khanhtran47 <tranduckhanh23@gmail.com>
2020-09-29 08:01:41 +0200

    adding react hooks (useState, useEffect)

---
+++ src/hooks/index.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-nested-ternary */
+import { useState, useEffect } from 'react';
+import { firebase } from '../firebase';
+
+const collatedTasksExist = () => {};
+
+export const useTasks = (selectedProject) => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    let unsubscribe = firebase
+      .firestore()
+      .collection('tasks')
+      .where('userId', '==', '7jey1lAmXcK9bz5vPoVo');
+
+    unsubscribe =
+      selectedProject && !collatedTasksExist(selectedProject)
+        ? (unsubscribe = unsubscribe.where('projectId', '==', selectedProject))
+        : selectedProject === 'TODAY'
+        ? (unsubscribe = unsubscribe.where(
+            'date',
+            '==',
+            moment().format('DD/MM/YYYY')
+          ))
+        : selectedProject === 'INBOX' || selectedProject === 0
+        ? (unsubscribe = unsubscribe.where('date', '==', ''))
+        : unsubscribe;
+  }, [selectedProject]);
+};
