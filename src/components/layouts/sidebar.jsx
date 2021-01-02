fc89fa64dde9c2c2e15a2792c603799ca0827c22
Khanhtran47 <tranduckhanh23@gmail.com>
2021-01-02 15:02:47 +0100

    adding Context to the Sidebar for Projects

--- src/components/layouts/sidebar.jsx
+++ src/components/layouts/sidebar.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import {
@@ -13,35 +16,62 @@ import { Projects } from '../Projects';
 import { AddProject } from '../AddProject';
 
 export const Sidebar = () => {
-  const { setSelectedProject } = useSelectedProjectValue;
+  const { setSelectedProject } = useSelectedProjectValue();
   const [active, setActive] = useState('inbox');
   const [showProjects, setShowProjects] = useState(true);
 
   return (
     <div className="sidebar" data-testid="sidebar">
       <ul className="sidebar__generic">
-        <li data-testid="inbox" className="inbox">
+        <li
+          data-testid="inbox"
+          className={active === 'inbox' ? 'active' : undefined}
+          onClick={() => {
+            setActive('inbox');
+            setSelectedProject('INBOX');
+          }}
+        >
           <span>
             <FaInbox />
           </span>
           <span>Inbox</span>
         </li>
-        <li data-testid="today" className="today">
+
+        <li
+          data-testid="today"
+          className={active === 'today' ? 'active' : undefined}
+          onClick={() => {
+            setActive('today');
+            setSelectedProject('TODAY');
+          }}
+        >
           <span>
             <FaRegCalendar />
           </span>
           <span>Today</span>
         </li>
-        <li data-testid="next_7" className="next_7">
+        <li
+          data-testid="next_7"
+          className={active === 'next_7' ? 'active' : undefined}
+          onClick={() => {
+            setActive('next_7');
+            setSelectedProject('NEXT_7');
+          }}
+        >
           <span>
             <FaRegCalendarAlt />
           </span>
           <span>Next 7 days</span>
         </li>
       </ul>
-      <div className="sidebar__middle">
+      <div
+        className="sidebar__middle"
+        onClick={() => setShowProjects(!showProjects)}
+      >
         <span>
-          <FaChevronDown />
+          <FaChevronDown
+            className={!showProjects ? 'hidden-projects' : undefined}
+          />
         </span>
         <h2>Projects</h2>
       </div>
